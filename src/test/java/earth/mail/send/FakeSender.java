65e6060ea27cc92c0e1d4074eab4634ff8456a28
garrison@example.com <cadet@10.0.1.28>
2014-03-05 14:59:04 +0800

    modify mailsender; change recipient type to Arraylist

--- src/test/java/earth/mail/send/FakeSender.java
+++ src/test/java/earth/mail/send/FakeSender.java
@@ -3,7 +3,9 @@ package earth.mail.send;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.awt.List;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.mail.Message;
 import javax.mail.Message.RecipientType;
@@ -20,16 +22,19 @@ public class FakeSender implements Sender {
 
     }
 
-    public void verifyMailSent(String topic, String body, String receiver,
-            String from) throws MessagingException, IOException {
+    public void verifyMailSent(String topic, String body,
+            ArrayList<String> receiver, String from) throws MessagingException,
+            IOException {
 
         assertTrue(isSent);
         assertEquals(topic, message.getSubject());
         assertEquals(body, message.getContent());
-        assertEquals(receiver,
-                message.getRecipients(RecipientType.TO)[0].toString());
-        assertEquals(from, message.getFrom()[0].toString());
+        for (int i = 0; i < receiver.size(); i++) {
+            assertEquals(receiver.get(i),
+                    message.getRecipients(RecipientType.TO)[i].toString());
+        }
 
+        assertEquals(from, message.getFrom()[0].toString());
     }
 
 }
