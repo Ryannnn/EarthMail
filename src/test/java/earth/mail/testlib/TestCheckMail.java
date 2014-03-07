c920fc8f09ef5b33e41509ba266a3e3cf569731a
Doncky <Doncky@example.com>
2014-03-07 13:20:42 +0800

    update a new robot test for wrong email address

--- src/test/java/earth/mail/testlib/TestCheckMail.java
+++ src/test/java/earth/mail/testlib/TestCheckMail.java
@@ -6,16 +6,19 @@ import static org.junit.Assert.fail;
 import javax.mail.MessagingException;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class TestCheckMail {
     CheckMail checkMail = new CheckMail();
 
     @Test
+    @Category({FastTests.class})
     public void checkMail() throws MessagingException {
         assertTrue(checkMail.mailExists("iamnotman88@126.com"));
     }
     
     @Test(expected=RuntimeException.class)
+    @Category({SlowTests.class})
     public void checkMailNonExist() throws MessagingException {
         checkMail.mailExists("xxxx8@126.com");
         fail();
